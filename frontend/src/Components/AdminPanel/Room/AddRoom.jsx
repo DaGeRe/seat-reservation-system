12a60b49533812f741776114cfd6ade498249e34
root <root@jus-srv-test30.justiz.sachsen.de>
2024-08-28 12:01:10 +0200

    Auf Branch click_image Zum Commit vorgemerkte Änderungen: 	neue Datei:     frontend/src/Components/AdminPanel/Room/AddRoom.css 	geändert:       frontend/src/Components/AdminPanel/Room/AddRoom.jsx
    Works more or less...

--- frontend/src/Components/AdminPanel/Room/AddRoom.jsx
+++ frontend/src/Components/AdminPanel/Room/AddRoom.jsx
@@ -3,9 +3,14 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
+import firstFloorImage from '../../../images/firstfloor.png';
+import secondFloorImage from '../../../images/secondfloor.png'; 
+import './AddRoom.css'; 
 import * as React from 'react';
 import { toast } from 'react-toastify';
 import { useTranslation } from "react-i18next";
+import { Dialog, DialogTitle, IconButton } from '@mui/material';
+import LaptopIcon from '@mui/icons-material/Laptop';
 
 export default function AddRoom({ addRoomModal }) {
   // The jwt.
@@ -14,8 +19,8 @@ export default function AddRoom({ addRoomModal }) {
   const [floor, setFloor] = React.useState('');
   const [status, setStatus] = React.useState('');
   const [type, setType] = React.useState('');
-  const [x, setX] = React.useState('');
-  const [y, setY] = React.useState('');
+  const [x, setX] = React.useState(0.0);
+  const [y, setY] = React.useState(0.0);
   // +---------+--------------+------+-----+---------+-------+
     // | Field   | Type         | Null | Key | Default | Extra |
     // +---------+--------------+------+-----+---------+-------+
@@ -97,25 +102,114 @@ export default function AddRoom({ addRoomModal }) {
     // | x       | int(11)      | NO   |     | NULL    |       |
     // | y       | int(11)      | NO   |     | NULL    |       |
     // +---------+--------------+------+-----+---------+-------+
+
+    const floorImage = floor === 'Ground' ? firstFloorImage : secondFloorImage;
+
     return (
         <React.Fragment>
             <DialogContent>
                 <Grid container >
+               
+                 
                   <Box sx={{ flexGrow: 1, padding: '10px' }}>
-                    <FormControl required={true} size="small" fullWidth>
+                    {/* <FormControl required={true} size="small" fullWidth>
                       <InputLabel id="demo-simple-select-label-floor">{t("floor")}</InputLabel>
                       <Select
                         labelId="demo-simple-select-label-floor"
                         id="demo-simple-select-floor"
                         value={floor}
                         label={t("floor")}
-                        onChange={(e)=>setFloor(e.target.value)}
+                        onChange={(e)=>{
+                          setFloor(e.target.value);}}
                       >
                         <MenuItem value={"First"}>{t("firstFloor").toUpperCase()}</MenuItem>
                         <MenuItem value={"Ground"}>{t("groundFloor").toUpperCase()}</MenuItem>
                       </Select>
-                    </FormControl>
-                    <br></br> <br></br>
+                    </FormControl> */}
+                    {/* <div className="image-wrapper"> */}
+                    <div
+                      onClick={(e) => {
+                        // setX(e.pageX - e.target.offsetLeft);
+                        // setY(e.pageY - e.target.offsetTop);
+                        // console.log('e.page', e.pageX, e.pageY);
+                        // console.log('e.target', e.target.offsetLeft, e.target.offsetTop);
+                        // console.log('client: ', e.clientX, e.clientY);
+                        // console.log('layer', e.layerX, e.layerY);
+                        // console.log('click: ', x, y);
+                        // //<Test>
+                        // setX(e.layerX);
+                        // setY(e.layerY);
+                        // //</Test>
+                        // console.log('---------------');
+                      }  
+                    }
+                    >
+                      <img id='myImage' className="myimage" src={floorImage} alt={`${floor === 'Ground' ? 'Ground' : 'First'} Floor`}
+                        onClick={(e)=>{
+                          // https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
+
+                          // const coordinatesDiv = document.getElementById('coordinates');
+                          // const img = document.getElementById('myImage');
+                          // const rect = img.getBoundingClientRect();
+                          // const rect_length = rect.right - rect.left;
+                          // const rect_height = rect.bottom - rect.top;
+                          // const x = e.clientX - rect.left;
+                          // const y = e.clientY - rect.top;
+                          // const xPercent = (x / rect.width) * 100;
+                          // const yPercent = (y / rect.height) * 100;
+                          // //setX(e.layerX);
+                          // //setY(e.layerY);
+                          // console.log(rect);
+                          // console.log('percent: ', xPercent, yPercent);
+                          // console.log('natural: ', img.naturalWidth, img.naturalHeight);
+                          
+                          // const xPx=(xPercent/100)* rect.right;
+                          // const yPx=(yPercent/100)*rect.bottom ;
+                          // setX(xPx);
+                          // setY(yPx);
+
+                          const img = document.getElementById('myImage');
+                          const rect = img.getBoundingClientRect();
+
+                          setX(rect.right);
+                          setY(rect.bottom);
+                          
+                          
+                          //coordinatesDiv.innerHTML = `Coordinates: (${xPercent.toFixed(2)}%, ${yPercent.toFixed(2)}%)`;
+                          
+                        }}
+                        onMouseMove={(e)=>{
+                          const img = document.getElementById('myImage');
+                          const rect = img.getBoundingClientRect();
+                          const currx = e.clientX - rect.left;
+                          const curry = e.clientY - rect.top;
+                          const xPercent = (currx / rect.width) * 100;
+                          const yPercent = (curry / rect.height) * 100;
+                          
+                          //console.log(xPercent, yPercent);
+                
+                          
+                          setX((xPercent/100)*rect.width);
+                          setX((e.pageX - e.target.offsetLeft));
+                          setY((yPercent/100)*rect.height);
+                          console.log(xPercent/100);
+                          console.log(rect.width);
+                          console.log(x);
+                          console.log('-------');
+                        }}
+                      />
+                      {/* { x != 0 && y != 0 && */}
+                      <a className="myicon" style={{
+                        top: `${y}px`, left: `${x}px` }}>
+                    <IconButton>
+                        <LaptopIcon />
+                      </IconButton>
+                      </a>
+                    {/* }  */}
+                    </div>
+          
+                    
+                    {/* <br></br> <br></br>
                     <FormControl required={true} fullWidth>
                       <InputLabel id="demo-simple-select-label">{t("type")}</InputLabel>
                       <Select
@@ -164,7 +258,7 @@ export default function AddRoom({ addRoomModal }) {
                         value={y}
                         onChange={(e)=>setY(e.target.value)}
                       />
-                    </FormControl>
+                    </FormControl> */}
 
                     
       <DialogActions>
@@ -173,113 +267,8 @@ export default function AddRoom({ addRoomModal }) {
       </DialogActions>
 
 
-            {/* <Autocomplete
-              id="tags-filled"
-              fullWidth
-              options={allRooms.map((option) => (option.floor +"-"+ option.type +"("+option.id+")"))}
-              value={selectedRoom}
-              onChange={(event, newValue) => {
-                  console.log(newValue);
-                  setSelectedRoom(
-                      newValue);
-              }}
-              renderInput={(params) => (
-                  <TextField
-                      {...params}
-                      variant="outlined"
-                      size='small' 
-                      label={t("selectRoom")}
-                      placeholder={t("selectRoom")}
-                  />
-              )}
-            />
-            <br></br>
-            <FormControl required={true} size="small" fullWidth variant="standard">
-              <TextField
-                id="standard-adornment-reason"
-                label={t("deskID")}
-                size="small"
-                type={"number"}
-                value={deskId}
-                onChange={(e)=>setDeskId(e.target.value)}
-              />
-            </FormControl>
-            <br></br><br></br>
-            <FormControl fullWidth size='small'>
-              <InputLabel id="demo-simple-select-label">{t("equipment")}</InputLabel>
-              <Select
-                size='small'
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={equipment}
-                placeholder='Equipments'
-                label="Equipments"
-                onChange={(e) => setEquipment(e.target.value)}
-              >
-                <MenuItem value={"with equipment"}>{t("withEquipment").toUpperCase()}</MenuItem>
-                <MenuItem value={"without equipment"}>{t("withoutEquipment").toUpperCase()}</MenuItem>
-              </Select>
-            </FormControl> */}
           </Box>
-                {/* <TableContainer  component={Paper}>
-      <Table sx={{ minWidth: 450, marginTop: 1, maxHeight:'400px' }} >
-        <TableHead sx={{backgroundColor: 'green', color:'white'}}>
-          <TableRow>
-          <TableCell sx={{textAlign: 'center', fontSize:15, color:'white'}}> ID</TableCell>
-             
-            <TableCell sx={{textAlign: 'center', fontSize:15, color:'white'}}>{t("floor")}</TableCell>
-            <TableCell sx={{textAlign: 'center', fontSize:15,color:'white' }}>{t("type")}</TableCell>
-             <TableCell sx={{textAlign: 'center',fontSize:15,color:'white' }} colSpan={2}>{t("action")}</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {allRooms.map((row) => (
-            <TableRow  key={row.categoryId}>
-              <TableCell sx={{textAlign: 'center', fontSize:14, fontWeight:400 }} component="th" scope="row">
-                {row.id}
-              </TableCell>
-              <TableCell sx={{textAlign: 'center', fontSize:14, fontWeight:400 }} >
-                {row.floor === "Ground" ? t("groundFloor") : t("firstFloor")}
-              </TableCell>
-              
-              <TableCell sx={{fontSize:14, width:'30%'   }} component="th" scope="row">
-              <FormControl fullWidth size='small'>
-                <InputLabel id="demo-simple-select-label">{t("type")}</InputLabel>
-                <Select
-                size='small'
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={row.type}
-                    label="Status"
-                    onChange={(e) => handleRoomTypeChange(e, row.id)}
-                >
-                    <MenuItem value={"Silence"}>{t("silence")}</MenuItem>
-                    <MenuItem value={"Normal"}>{t("normal")}</MenuItem>
-                </Select>
-                </FormControl>
-             </TableCell>
-              
-              <TableCell sx={{fontSize:14, width:'30%'   }} component="th" scope="row">
-              <FormControl fullWidth size='small'>
-                <InputLabel id="demo-simple-select-label">{t("status")}</InputLabel>
-                <Select
-                size='small'
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={row.status?row.status:"enable"}
-                    label="Status"
-                    onChange={(e) => handleStatusChange(e, row.id)}
-                >
-                    <MenuItem value={"enable"}>{t("enable").toUpperCase()}</MenuItem>
-                    <MenuItem value={"disable"}>{t("disable").toUpperCase()}</MenuItem>
-                </Select>
-                </FormControl>
-             </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer> */}
+                
                     </Grid>
 
             </DialogContent>
