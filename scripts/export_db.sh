fef1dd38913235a1537ac866c29cbefdb3444640
root <root@jus-srv-test30.justiz.sachsen.de>
2024-09-14 13:11:38 +0200

    Import export works.

--- scripts/export_db.sh
+++ scripts/export_db.sh
@@ -7,6 +7,4 @@ fi
 
 outputfile="dumps/mydatabase_dump.sql"
 
-echo "SET FOREIGN_KEY_CHECKS=0;" > $outputfile
 docker exec $container mariadb-dump --add-drop-table --complete-insert -u root -pmypasss --databases mydatabase >> $outputfile
-echo "FOREIGN_KEY_CHECKS=1;" >> $outputfile
--- scripts/import_db.sh
+++ scripts/import_db.sh
@@ -7,9 +7,6 @@ fi
 
 container="$1"
 dumpfile="$2"
+database="mydatabase"
 
-#docker exec $container mariadb-dump -u root -pmypasss mydatabase > dumps/mydatabase_dump.sql
-#cat $dumpfile | docker exec $container mariadb-dump -u root -pmypasss mydatabase
-docker exec $container  mariadb-dump  -u root -pmypasss mydatabase < $dumpfile --force
-
-#cat /root/DeskSharingTool_Dev/dumps/mydatabase_dump.sql
+cat $dumpfile | docker exec -i $container mariadb -pmypasss  $database
