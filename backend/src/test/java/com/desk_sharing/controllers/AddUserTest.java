8fad034d800b09dd6a0d7370e6bca3c53f658d30
root <root@jus-srv-test30.justiz.sachsen.de>
2024-09-23 19:00:59 +0200

    Add basic unit test settings.

---
+++ backend/src/test/java/com/desk_sharing/controllers/AddUserTest.java
@@ -0,0 +1,29 @@
+package com.desk_sharing.controllers;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNull;
+// import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+// import org.mockito.InjectMocks;
+// import org.mockito.Mock;
+// import org.mockito.MockitoAnnotations;
+// import org.springframework.security.crypto.password.PasswordEncoder;
+
+// import static org.mockito.Mockito.when;
+
+// import com.desk_sharing.repositories.UserRepository;
+// import com.desk_sharing.services.UserService;
+
+// import java.util.Arrays;
+// import java.util.List;
+// import java.util.Optional;
+
+public class AddUserTest {
+
+    @Test
+    public void testFindByEmail_NotFound() {
+     
+        // Verify the result
+        assertNull(null); 
+    }
+}
