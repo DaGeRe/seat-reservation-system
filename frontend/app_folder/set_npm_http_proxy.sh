24a60749fc7765b09e82cbd737447a474fe5cc8a
Your Name <you@example.com>
2024-10-16 16:16:33 +0200

    Some updates to satisfy the linter.

--- frontend/app_folder/set_npm_http_proxy.sh
+++
@@ -1,6 +0,0 @@
-npm config set proxy http://proxy.justiz.sachsen.de:3128
-#npm config get https-proxy
-#npm config delete proxy
-
-#npm config set registry http://registry.npmjs.org/
-#npm config set registry https://registry.npmjs.org/
