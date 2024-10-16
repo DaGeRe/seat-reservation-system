893a63ad246d7ee0f2d6d672586db5df62433f1a
Your Name <you@example.com>
2024-10-16 12:11:23 +0200

    New context for frontend works.

---
+++ scripts/set_npm_http_proxy.sh
@@ -0,0 +1,6 @@
+npm config set proxy http://proxy.justiz.sachsen.de:3128
+#npm config get https-proxy
+#npm config delete proxy
+
+#npm config set registry http://registry.npmjs.org/
+#npm config set registry https://registry.npmjs.org/
