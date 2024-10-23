f9f19a294684473506967043eb43fd14dc18ef30
Your Name <you@example.com>
2024-10-23 17:31:57 +0200

    RoomController functions uses logging.

--- backend/src/main/java/com/desk_sharing/controllers/RoomController.java
+++ backend/src/main/java/com/desk_sharing/controllers/RoomController.java
@@ -2,6 +2,8 @@ package com.desk_sharing.controllers;
 
 import com.desk_sharing.entities.Room;
 import com.desk_sharing.services.RoomService;
+import com.desk_sharing.services.UserService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,26 +19,33 @@ public class RoomController {
     @Autowired
     RoomService roomService;
 
+    @Autowired
+    UserService userService;
+
     @PostMapping("/create")
     public ResponseEntity<Room> createRoom(@RequestBody Room room) {
+        userService.logging("createRoom( " + room + " )");
         Room savedRoom = roomService.saveRoom(room);
         return new ResponseEntity<>(savedRoom, HttpStatus.CREATED);
     }
 
     @GetMapping
     public ResponseEntity<List<Room>> getAllRooms() {
+        userService.logging("getAllRooms()");
         List<Room> rooms = roomService.getAllRooms();
         return new ResponseEntity<>(rooms, HttpStatus.OK);
     }
     
     @GetMapping("/status")
     public ResponseEntity<List<Room>> getAllRoomsByActiveStatus() {
+        userService.logging("getAllRoomsByActiveStatus()");
         List<Room> rooms = roomService.getAllRoomsByActiveStatus();
         return new ResponseEntity<>(rooms, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Room> getRoomById(@PathVariable("id") Long id) {
+        userService.logging("getRoomById( + " + id + " )");
         Optional<Room> room = roomService.getRoomById(id);
         return room.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
                 .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
@@ -44,42 +53,49 @@ public class RoomController {
 
     @PutMapping("/{id}")
     public ResponseEntity<Room> updateRoom(@PathVariable("id") Long id, @RequestBody Room room) {
+        userService.logging("updateRoom( + " + id + ", " + room + " )");
         Room updatedRoom = roomService.updateRoom(id, room);
         return new ResponseEntity<>(updatedRoom, HttpStatus.OK);
     }
 
     @PutMapping("/{id}/floor/{floor}")
     public ResponseEntity<Room> updateRoomFloor(@PathVariable("id") Long id, @PathVariable("floor") String floor) {
+        userService.logging("updateRoomFloor( + " + id + ", " + floor + " )");
         Room updatedRoom = roomService.updateRoomFloor(id, floor);
         return new ResponseEntity<>(updatedRoom, HttpStatus.OK);
     }
 
     @PutMapping("/{id}/remark/{remark}")
     public ResponseEntity<Room> updateRoomRemark(@PathVariable("id") Long id, @PathVariable("remark") String remark) {
+        userService.logging("updateRoomRemark( + " + id + ", " + remark + " )");
         Room updatedRoom = roomService.updateRoomRemark(id, remark);
         return new ResponseEntity<>(updatedRoom, HttpStatus.OK);
     }
     
     @PutMapping("/{id}/type/{type}")
     public ResponseEntity<Room> updateRoomType(@PathVariable("id") Long id, @PathVariable("type") String type) {
+        userService.logging("updateRoomType( + " + id + ", " + type + " )");
         Room updatedRoom = roomService.updateRoomType(id, type);
         return new ResponseEntity<>(updatedRoom, HttpStatus.OK);
     }
     
     @PutMapping("/{id}/{status}")
     public ResponseEntity<Room> updateRoomStatus(@PathVariable("id") Long id, @PathVariable("status") String status) {
+        userService.logging("updateRoomStatus( + " + id + ", " + status + " )");
         Room updatedRoom = roomService.updateRoomStatus(id, status);
         return new ResponseEntity<>(updatedRoom, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Integer> deleteRoom(@PathVariable("id") Long id) {
+        userService.logging("deleteRoom( + " + id + " )");
         int ret = roomService.deleteRoom(id);
         return ResponseEntity.status(HttpStatus.OK).body(ret);
     } 
     
     @DeleteMapping("/ff/{id}")
     public boolean deleteRoomFf(@PathVariable("id") Long id) {
+        userService.logging("deleteRoomFf( + " + id + " )");
         return roomService.deleteRoomFf(id);
     }
 }
