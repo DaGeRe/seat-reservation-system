f55780615d52f80de90d1adad8297c2e2826399c
Your Name <you@example.com>
2024-10-28 11:35:44 +0100

    Uses 5.8.15 for spring-security.

--- backend/src/main/java/com/desk_sharing/DeskSharingToolApplication.java
+++ backend/src/main/java/com/desk_sharing/DeskSharingToolApplication.java
@@ -9,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class DeskSharingToolApplication {
 
 	public static void main(String[] args) {
+		//System.out.println("Spring Framework Version: " + SpringVersion.getVersion());
 		SpringApplication.run(DeskSharingToolApplication.class, args);
 	}
 }
