fda77f0327488fa4d7976ee18144fdc9c611295a
Your Name <you@example.com>
2024-10-28 11:43:46 +0100

    security 5.8 seems to be used.

--- backend/install.sh
+++ backend/install.sh
@@ -1,4 +1,9 @@
 mvn clean install -U -DskipTests -Dmaven.repo.local=/home/r/DeskSharingTool_Dev/backend/.m2/repository
 mvn clean package -DskipTests -Dmaven.repo.local=/home/r/DeskSharingTool_Dev/backend/.m2/repository
 mvn dependency:go-offline -Dmaven.repo.local=/home/r/DeskSharingTool_Dev/backend/.m2/repository
+
+mvn clean install -U -DskipTests
+mvn clean package -DskipTests
+mvn dependency:go-offline
+
 mvn dependency:tree # fehler im pom finden
