52e259db84cd185e5e5c1f5956668b34e4af2af2
Your Name <you@example.com>
2024-10-29 08:44:54 +0100

    Desk has an extra column that increments for each desk per room.

--- backend/src/main/java/com/desk_sharing/entities/Room.java
+++ backend/src/main/java/com/desk_sharing/entities/Room.java
@@ -1,11 +1,5 @@
 package com.desk_sharing.entities;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
-import com.desk_sharing.services.DeskService;
-
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -40,83 +34,9 @@ public class Room {
     @Column(name = "remark")
     private String remark;
 
-    /** We dont want to have a column with rooms. */
-    @Transient
-    private List<Desk> desks;
-
-/*     @Transient
-    @Autowired
-    private DeskService deskService; */
-
     public Room(String type, int x, int y) {
         this.type = type;
         this.x = x;
         this.y = y;
     }
-
-/*     @PostLoad
-    public void initNonPersistentFields() {
-        this.desks = deskService.getDeskByRoomId(id);
-    } */
-
-/*     public List<Desk> getDesks() {
-        return deskService.getDeskByRoomId(id);
-    }  */
-    
-
-   /*  public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    public String getFloor() {
-        return floor;
-    }
-
-    public void setFloor(String floor) {
-        this.floor = floor;
-    }
-
-	public String getStatus() {
-		return status;
-	}
-
-	public void setStatus(String status) {
-		this.status = status;
-	}
-
-	public String getRemark() {
-		return remark;
-	}
-
-	public void setRemark(String remark) {
-		this.remark = remark;
-	} */
 }
