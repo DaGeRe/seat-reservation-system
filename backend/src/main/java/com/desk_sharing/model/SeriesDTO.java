9a50f59f182c91eed5bbdbe624f68d1a5d0697db
Your Name <you@example.com>
2024-11-23 18:11:54 +0100

    SeriesDTO added.

---
+++ backend/src/main/java/com/desk_sharing/model/SeriesDTO.java
@@ -0,0 +1,20 @@
+package com.desk_sharing.model;
+
+import java.sql.Date;
+import java.sql.Time;
+
+import com.desk_sharing.entities.Desk;
+import com.desk_sharing.entities.Room;
+import com.desk_sharing.entities.UserEntity;
+
+public class SeriesDTO {
+    private Long id;
+    private Date startDate;
+    private Date endDate;
+    private Time startTime;
+    private Time endTime;
+    private String frequency;
+    private UserEntity user;
+    private Room room;
+    private Desk desk;
+}
