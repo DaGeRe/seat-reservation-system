68d5bc7c7145d27f106b9bf209d3ee00e0059cbd
Your Name <you@example.com>
2024-11-24 12:41:53 +0100

    Extracted dates works so far.

--- backend/src/main/java/com/desk_sharing/repositories/SeriesRepository.java
+++ backend/src/main/java/com/desk_sharing/repositories/SeriesRepository.java
@@ -13,51 +13,51 @@ public interface SeriesRepository extends JpaRepository<Series, Long> {
     @Query(
         value = ""
         + " WITH RECURSIVE days AS ( "
-        + "     SELECT CAST(:dayStart AS DATE) AS curr_Date "
+        + "     SELECT CAST(:startDate AS DATE) AS curr_Date "
         + "     UNION ALL "
         + "     SELECT DATE_ADD(curr_Date, INTERVAL 1 DAY) "
         + "     FROM days "
-        + "     WHERE DATE_ADD(curr_Date, INTERVAL 1 DAY) <= CAST(:dayEnd AS DATE) "
+        + "     WHERE DATE_ADD(curr_Date, INTERVAL 1 DAY) <= CAST(:endDate AS DATE) "
         + " ) "
         + " SELECT curr_Date from days "
         ,nativeQuery = true
     )
     public List<Date> getDaily(
-        @Param("dayStart") Date dayStart,
-        @Param("dayEnd") Date dayEnd
+        @Param("startDate") Date startDate,
+        @Param("endDate") Date endDate
     );
 
     @Query(
         value = ""
         + " WITH RECURSIVE days AS ( "
-        + "     SELECT CAST(:dayStart AS DATE) AS curr_Date "
+        + "     SELECT CAST(:startDate AS DATE) AS curr_Date "
         + "     UNION ALL "
         + "     SELECT DATE_ADD(curr_Date, INTERVAL 1 WEEK) "
         + "     FROM days "
-        + "     WHERE DATE_ADD(curr_Date, INTERVAL 1 WEEK) <= CAST(:dayEnd AS DATE) "
+        + "     WHERE DATE_ADD(curr_Date, INTERVAL 1 WEEK) <= CAST(:endDate AS DATE) "
         + " ) "
         + " SELECT curr_Date from days "
         ,nativeQuery = true
     )
     public List<Date> getWeekly(
-        @Param("dayStart") Date dayStart,
-        @Param("dayEnd") Date dayEnd
+        @Param("startDate") Date startDate,
+        @Param("endDate") Date endDate
     );
 
     @Query(
         value = ""
         + " WITH RECURSIVE days AS ( "
-        + "     SELECT CAST(:dayStart AS DATE) AS curr_Date "
+        + "     SELECT CAST(:startDate AS DATE) AS curr_Date "
         + "     UNION ALL "
         + "     SELECT DATE_ADD(curr_Date, INTERVAL 1 MONTH) "
         + "     FROM days "
-        + "     WHERE DATE_ADD(curr_Date, INTERVAL 1 MONTH) <= CAST(:dayEnd AS DATE) "
+        + "     WHERE DATE_ADD(curr_Date, INTERVAL 1 MONTH) <= CAST(:endDate AS DATE) "
         + " ) "
         + " SELECT curr_Date from days "
         ,nativeQuery = true
     )
     public List<Date> getMonthly(
-        @Param("dayStart") Date dayStart,
-        @Param("dayEnd") Date dayEnd
+        @Param("startDate") Date startDate,
+        @Param("endDate") Date endDate
     );
 }
