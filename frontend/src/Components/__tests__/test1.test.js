e1929a497864cd216c179099bcb0a83c37ad46af
Your Name <you@example.com>
2025-01-03 08:51:07 +0100

    Basic test works.

--- frontend/src/Components/__tests__/test1.test.js
+++ frontend/src/Components/__tests__/test1.test.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../../App'; // Adjust the path to your App component
+import '@testing-library/jest-dom'; // für bessere Matcher wie `toBeInTheDocument`
+import { Experimental_CssVarsProvider } from '@mui/material';
 
 test('renders the landing page', () => {
   render(<App />);
-  //expect(screen.getByText(/some text/i)).toBeInTheDocument(); // Adjust assertion as needed
+  // Sucht alle Elemente mit dem Text "Login"
+  const loginElements = screen.getAllByText(/Login/i);
+  
+  // Überprüft, ob mindestens eines vorhanden ist
+  expect(loginElements.length).toBeGreaterThan(0);
 });
