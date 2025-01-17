f312347a5165be90bd28d82462ed48f9d7a5de54
Your Name <you@example.com>
2025-01-17 12:18:41 +0100

    Some more tests.

--- cypress/cypress/integration/sample.cy.js
+++ cypress/cypress/integration/sample.cy.js
@@ -107,27 +107,6 @@
       cy.get('[data-testid="password"]').should('exist').type('test');
       cy.get('[data-testid="loginBtn"]').click();
       
-      // Wait for the response.
-      /*cy.wait('@loginRequest').then((interception) => {
-        const response = interception.response;
-        expect(response === null).to.equal(false);
-        const statusCode = response.statusCode;
-        expect(statusCode).to.equal(200);
-        const body = response.body;
-        const accessToken = response.body.accessToken;
-        expect(accessToken === null).to.equal(false);
-
-        cy.get('[data-testid="loginErrorMsg"]').should('not.exist');
-        // Token im lokalen Speicher speichern
-        cy.window().then((window) => {
-          //window.localStorage.setItem('authToken', accessToken);
-          //cy.task('log', `localStorage.getItem : ${window.localStorage.getItem('headers')}`);
-          window.localStorage.setItem('headers', JSON.stringify({
-          'Authorization': 'Bearer ' +  String(accessToken),
-          'Content-Type': 'application/json',
-        }));
-        });
-      });*/
       login();
 
       // After an successful login we have to be on the next page.
@@ -157,6 +136,33 @@
       // This elment only exists if a data is pulled from the backend.
       cy.get('.rbc-row-segment').should('exist');
       cy.screenshot('check_if_calendar_events_are_loaded.png');
+    });
+  });
+
+  describe('Test the /home page', () => {
+    it('checks all of the gui elements in SidebarComponent', ()=> {
+      cy.visit('/home');
+
+      cy.get('[data-testid="SidebarComponent"]').should('exist');
+      cy.get('[data-testid="SidebarComponent_Collapse"]').should('exist');
+      cy.get('[data-testid="SidebarComponent_Collapse"]').should('exist');
+      //tbc
+    });
+
+    it('check if calendar events are loaded', ()=> {
+      login()
+      cy.visit('/home');
+      cy.url().should('include', '/home');
+      
+      cy.get('.rbc-day-bg').should('exist');
+    });
+
+    it('create a booking', ()=> {
+      login()
+      cy.visit('/home');
+      cy.url().should('include', '/home');
+      
+      cy.get('rbc-day-bg rbc-today').should('exist');
     });
   });
 
