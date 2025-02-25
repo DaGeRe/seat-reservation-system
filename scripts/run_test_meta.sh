62a1f0f4ac3f438db1e311dad56099844a5efbf9
Your Name <you@example.com>
2025-02-25 08:05:01 +0100

    Rm old folder.

--- scripts/run_test_meta.sh
+++ scripts/run_test_meta.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 #if [ -z "$1" ]; then
-for i in {1..7}; do
+for i in {1..15}; do
   #./scripts/run_test.sh 2>&1 | sed -n 's/^.*abcde //p'
   #./scripts/run_test.sh "cypress/integration/$1" 2>&1 | sed -n 's/^.*failed //p'
   echo "${i}th iteration"
