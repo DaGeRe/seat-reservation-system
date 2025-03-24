0b1b64690f1e18530fe82e32861b8196d00fa5df
Your Name <you@example.com>
2025-03-24 12:57:32 +0100

    Add Constructor that takes an booking to BookingDTO.java

--- backend/src/main/java/com/desk_sharing/model/BookingDTO.java
+++ backend/src/main/java/com/desk_sharing/model/BookingDTO.java
@@ -2,6 +2,9 @@ package com.desk_sharing.model;
 
 import java.sql.Date;
 import java.sql.Time;
+import java.util.List;
+
+import com.desk_sharing.entities.Booking;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,7 +21,17 @@ public class BookingDTO {
     private Time begin;
     private Time end;
 
-
+    public BookingDTO(final Booking booking) {
+        this(
+            booking.getId(),
+            booking.getUser().getId(),
+            booking.getRoom().getId(),
+            booking.getDesk().getId(),
+            booking.getDay(),
+            booking.getBegin(),
+            booking.getEnd()
+        );
+    };
     
 
 }
