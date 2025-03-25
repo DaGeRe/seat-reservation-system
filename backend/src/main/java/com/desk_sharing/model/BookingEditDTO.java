7ea7e6b18348d81f344a00f85ffa282497e1535c
Your Name <you@example.com>
2025-03-25 15:12:21 +0100

    Add @Data annotation to BookingEditDTO and DeskDTO.

--- backend/src/main/java/com/desk_sharing/model/BookingEditDTO.java
+++ backend/src/main/java/com/desk_sharing/model/BookingEditDTO.java
@@ -2,26 +2,11 @@ package com.desk_sharing.model;
 
 import java.sql.Time;
 
+import lombok.Data;
+
+@Data
 public class BookingEditDTO {
 	private Long id;
     private Time begin;
     private Time end;
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public Time getBegin() {
-		return begin;
-	}
-	public void setBegin(Time begin) {
-		this.begin = begin;
-	}
-	public Time getEnd() {
-		return end;
-	}
-	public void setEnd(Time end) {
-		this.end = end;
-	}
 }
--- backend/src/main/java/com/desk_sharing/model/DeskDTO.java
+++ backend/src/main/java/com/desk_sharing/model/DeskDTO.java
@@ -1,40 +1,12 @@
 package com.desk_sharing.model;
 
+import lombok.Data;
+
+@Data
 public class DeskDTO {
 
 	private Long roomId;
 	private String equipment;
 	private String remark;
 	private Long deskId;
-
-	public Long getRoomId() {
-		return roomId;
-	}
-
-	public void setRoomId(Long roomId) {
-		this.roomId = roomId;
-	}
-
-	public String getEquipment() {
-		return equipment;
-	}
-	
-	public void setEquipment(String equipment) {
-		this.equipment = equipment;
-	}
-	public String getRemark() {
-		return remark;
-	}
-	
-	public void setRemark(String remark) {
-		this.remark = remark;
-	}
-
-	public Long getDeskId() {
-		return deskId;
-	}
-
-	public void setDeskId(Long deskId) {
-		this.deskId = deskId;
-	}
 }
