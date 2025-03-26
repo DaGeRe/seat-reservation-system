dc060ce81ef9d0c7487ebc74d2993ba895b0436f
Your Name <you@example.com>
2025-03-26 12:03:00 +0100

    Delete data_migration project and some references to it since the migration already took place.

--- backend/src/main/java/com/desk_sharing/controllers/SeriesController.java
+++ backend/src/main/java/com/desk_sharing/controllers/SeriesController.java
@@ -4,14 +4,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.desk_sharing.entities.Desk;
-import com.desk_sharing.entities.Room;
-import com.desk_sharing.entities.Series;
 import com.desk_sharing.model.DatesAndTimesDTO;
 import com.desk_sharing.model.RangeDTO;
 import com.desk_sharing.model.SeriesDTO;
-import com.desk_sharing.model.SeriesDTOWithDeskRemark;
-import com.desk_sharing.repositories.DeskRepository;
-import com.desk_sharing.repositories.SeriesRepository;
 import com.desk_sharing.services.SeriesService;
 import com.desk_sharing.services.UserService;
 
@@ -32,12 +27,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class SeriesController {
     @Autowired 
     private SeriesService seriesService;
-
-    @Autowired 
-    private SeriesRepository seriesRepository;
-    
-    @Autowired
-    private DeskRepository deskRepository;
 
     @Autowired
     private UserService userService;
@@ -61,55 +50,6 @@ public class SeriesController {
     public ResponseEntity<Boolean> createSeries(@RequestBody SeriesDTO seriesDto) {
         userService.logging("createSeries( " + seriesDto + " )");
         return new ResponseEntity<Boolean>(seriesService.createSeries(seriesDto), HttpStatus.OK);
-    }
-
-    @PostMapping("/createSeriesForDeskRemark")
-    public ResponseEntity<String> createSeriesForDeskRemark(@RequestBody SeriesDTOWithDeskRemark seriesDTOWithDeskRemark) {
-        userService.logging("createSeriesForDeskRemark()");
-        final String deskRemark = seriesDTOWithDeskRemark.getDeskRemark();
-        final Desk desk = deskRepository.findByDeskRemark(deskRemark);
-        if (desk == null) {
-            return new ResponseEntity<String>("cannot find desk", HttpStatus.OK);
-        }
-        final Room room = desk.getRoom();
-        if (room == null) {
-            return new ResponseEntity<String>("cannot find room", HttpStatus.OK);
-        }
-        final SeriesDTO seriesDTO = new SeriesDTO(
-            0L, 
-            seriesDTOWithDeskRemark.getRangeDTO(), 
-            seriesDTOWithDeskRemark.getDates(), 
-            room, 
-            desk,
-            seriesDTOWithDeskRemark.getEmail()
-        );
-
-        // Check if there is allready an series.
-        final List<Series> existingSeries = seriesRepository.getAllSeriesForPreventDuplicates(
-            seriesDTOWithDeskRemark.getRangeDTO().getStartDate(),
-            seriesDTOWithDeskRemark.getRangeDTO().getEndDate(),
-            seriesDTOWithDeskRemark.getRangeDTO().getStartTime(),
-            seriesDTOWithDeskRemark.getRangeDTO().getEndTime(),
-            room.getId(),
-            desk.getId(),
-            seriesDTOWithDeskRemark.getEmail()
-        );
-        System.out.println("seriesDTOWithDeskRemark.getRangeDTO().getStartDate(): " + seriesDTOWithDeskRemark.getRangeDTO().getStartDate());
-        System.out.println("existingSeries.size(): " + existingSeries.size());
-
-        if (existingSeries.size()>0) {
-            return new ResponseEntity<String>("series already there", HttpStatus.OK);
-        }
-
-        final boolean ret = seriesService.createSeries(seriesDTO);
-        if (ret) {
-            return new ResponseEntity<String>("OK", HttpStatus.OK);
-        }
-        else {
-            return new ResponseEntity<String>("cannot create series", HttpStatus.OK);
-        }
-        //return new ResponseEntity<Boolean>(seriesService.createSeries(seriesDto), HttpStatus.OK);
-        
     }
     
     @GetMapping("/{email}")
