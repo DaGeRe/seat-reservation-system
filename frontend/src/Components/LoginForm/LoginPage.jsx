6aa1beadf19f8bff8808817eed6cafe27751411e
Your Name <you@example.com>
2025-08-22 08:56:06 +0200

    Outsource sx style definitions from LoginPage.jsx to LoginForm.style.js.

--- frontend/src/Components/LoginForm/LoginPage.jsx
+++ frontend/src/Components/LoginForm/LoginPage.jsx
@@ -1,4 +1,4 @@
-import { useReducer/*, useState*/ } from 'react';
+import { useReducer } from 'react';
 import './LoginPage.css';
 import { Box, Button,  FormControl, OutlinedInput, InputAdornment } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
@@ -6,14 +6,12 @@ import { useTranslation } from 'react-i18next';
 import { FaUser } from 'react-icons/fa';
 import { FaLock } from 'react-icons/fa';
 import { toast } from 'react-toastify';
+import { styles } from './LoginForm.style';
 import isEmail from '../misc/isEmail';
 import InfoModal from '../InfoModal';
 const LoginPage = () => {
   const { t } = useTranslation();
   const navigate = useNavigate();
-  //const [email, setEmail] = useState('');
-  //const [password, setPassword] = useState('');
-  //const [loginError, setLoginError] = useState('');
   const initState = {
     email: '',
     password: '',
@@ -32,7 +30,6 @@ const LoginPage = () => {
 
   const [state, dispatch] = useReducer(reducer, initState);
 
-  //const news = '<br/><ul><li>Anmeldung mit Windowskennung ist möglich</li><li>Festlegen von Standardetage möglich</li><li>Es kann nach Buchungen von Kollegen gesucht werden</li><li>Es kann nach Räumen gemäß der Kapazität gesucht werden</li><li>Der Anwender kann seine Standardansicht in der Buchungsübersicht wählen</li></ul>';
   const news = '<style>'+
   '.newslistlist ul {'+
     'margin-left: 20px;'+
@@ -54,37 +51,10 @@ const LoginPage = () => {
                 '   </ul>' +
                 ' </li>' +
                 '</ul>';
-  const wrapper_sx = {
-    width: '420px',
-    height: '370px',
-    background: 'transparent',
-    border: '2px solid rgba(0, 0, 0, 0.2)',
-    backdropFilter: 'blur(30px)',
-    boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)',
-    color: '#FFDD00',
-    borderRadius: '10px',
-    padding: '30px 40px',
-    position: 'relative'
-  };
-
-  const button_sx = {
-    width: '100%',
-    height: '45px',
-    background: '#fff',
-    border: 'none',
-    outline: 'none',
-    borderRadius: '40px',
-    boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
-    fontSize: '16px',
-    color: '#333',
-    fontWeight: 700,
-  }
-
 
   async function login() {
 
     if (!isEmail(state.email.trim())) {
-      //setLoginError(t('invalidEmail'));
       dispatch({ type: 'updateField', field: 'loginError', value: t('invalidEmail')});
       return;
     }
@@ -115,27 +85,20 @@ const LoginPage = () => {
         sessionStorage.setItem('accessToken', String(data['accessToken']));
         navigate('/home', { replace: true });
       } else {
-        //setLoginError(t('invalidCredentials'));
         dispatch({ type: 'updateField', field: 'loginError', value: t('invalidCredentials')})
         return;
       }
     } catch (error) {
       toast.error(t('loginFailed'));
-      //setLoginError(t('loginFailed'));
       dispatch({ type: 'updateField', field: 'loginError', value: t('loginFailed')});
     }
   }
 
   return (
     <>
-    <Box sx={wrapper_sx}>
-      <Box sx={{
-          flexGrow: 1,
-          overflowY: 'auto',
-          px: 3, // paddingX
-          py: 2, // paddingY
-      }}>
-        <h1 style={{margin: '20px', textAlign: 'center',}}>{t('login')}</h1>
+    <Box sx={styles.wrapper_sx}>
+      <Box sx={styles.box}>
+        <h1 style={styles.h1}>{t('login')}</h1>
         <br/>
         <img src={'/Assets/flag.png'} alt='Flag' className='flag-image' />
         <FormControl fullWidth required size='small'>
@@ -143,29 +106,16 @@ const LoginPage = () => {
             id='email'
             type='text'
             value={state.email}
-            //onChange={(e) => setEmail(e.target.value)}
             onChange={e => dispatch({type: 'updateField', field: 'email', value: e.target.value})}
             endAdornment={
               <InputAdornment
                 position='end'
-                sx={{
-                  backgroundColor: '#eef4ff',
-                  borderTopRightRadius: '4px',
-                  borderBottomRightRadius: '4px',
-                  px: 1,
-                  display: 'flex',
-                  alignItems: 'center',
-                }}
+                sx={styles.inputAdornment_sx}
               >
                 <FaUser/>
               </InputAdornment>
             }
-            sx={{
-              backgroundColor: '#eef4ff',
-              '& .MuiOutlinedInput-notchedOutline': {
-                borderColor: 'rgba(0,0,0,0.23)',
-              },
-            }}
+            sx={styles.outlinedInput_sx}
           />
         </FormControl>
         <br/><br/>
@@ -174,37 +124,23 @@ const LoginPage = () => {
           <OutlinedInput
             id='password'
             type='password'
-            //value={password}
             value={state.password}
-            //onChange={(e) => setPassword(e.target.value)}
             onChange={e => dispatch({type: 'updateField', field: 'password', value:e.target.value})}
             endAdornment={
               <InputAdornment
                 position='end'
-                sx={{
-                  backgroundColor: '#eef4ff', // ⬅️ identisch zum Feld
-                  borderTopRightRadius: '4px',
-                  borderBottomRightRadius: '4px',
-                  px: 1,
-                  display: 'flex',
-                  alignItems: 'center',
-                }}
+                sx={styles.inputAdornment_sx}
               >
                 <FaLock/>
               </InputAdornment>
             }
-            sx={{
-              backgroundColor: '#eef4ff', // ⬅️ wichtig!
-              '& .MuiOutlinedInput-notchedOutline': {
-                borderColor: 'rgba(0,0,0,0.23)',
-              },
-            }}
+            sx={styles.outlinedInput_sx}
           />
         </FormControl>
         <br/><br/>
 
         {state.loginError && <div data-testid='loginErrorMsg' className='error'>{state.loginError}</div>}
-        <Button id='login_btn' type='submit'  color='primary' onClick={login} sx={button_sx}>
+        <Button id='login_btn' type='submit'  color='primary' onClick={login} sx={styles.button_sx}>
             {t('login')}
         </Button>
         
