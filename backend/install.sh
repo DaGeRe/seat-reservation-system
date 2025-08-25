a2bea967ed011d48ad8e2423cc17d29e8121cd02
Your Name <you@example.com>
2025-08-25 08:46:44 +0200

    Up backend/install.sh. Rm the local maven repo.

--- backend/install.sh
+++ backend/install.sh
@@ -1,8 +1,12 @@
 . ../.env
 
-mvn clean install -U -DskipTests -Dmaven.repo.local=${PROJECT_PATH}/backend/.m2/repository
-mvn clean package -DskipTests -Dmaven.repo.local=${PROJECT_PATH}/backend/.m2/repository
-mvn dependency:go-offline -Dmaven.repo.local=${PROJECT_PATH}/backend/.m2/repository
+#mvn clean install -U -DskipTests -Dmaven.repo.local=${PROJECT_PATH}/backend/.m2/repository
+#mvn clean package -DskipTests -Dmaven.repo.local=${PROJECT_PATH}/backend/.m2/repository
+#mvn dependency:go-offline -Dmaven.repo.local=${PROJECT_PATH}/backend/.m2/repository
+
+mvn clean install -U -DskipTests
+mvn clean package -DskipTests 
+mvn dependency:go-offline 
 
 mvn clean install -U -DskipTests
 mvn clean package -DskipTests
