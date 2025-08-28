4ecbf1a12af26099ce67a0c08080da323656d9de
Your Name <you@example.com>
2025-08-28 15:44:54 +0200

    - Add scripts/save.sh for image tagging and exporting. - Outcomment some unused RUN staments in backend/Dockerfile. Also maven:3.8.4-openjdk-17-slim instead of maven:3.8.4-openjdk-17. - Rm frontend/package copy.json

--- scripts/save.sh
+++ scripts/save.sh
@@ -1,4 +1,12 @@
 docker save desksharingtool_dev-backend:latest -o desksharingtool_dev-backend.bin && \
 docker save desksharingtool_dev-frontend:latest -o desksharingtool_dev-frontend.bin && \
 docker save desksharingtool_dev-database -o desksharingtool_dev-database.bin && \
+
+docker tag desksharingtool_dev-frontend:latest localhost:5000/desksharingtool-dev-frontend:latest 
+docker push localhost:5000/desksharingtool-dev-frontend:latest
+
+#docker tag desksharingtool_dev-frontend:latest lcr.justiz.sachsen.de/desksharingtool-dev-frontend:latest
+#ocker push lcr.justiz.sachsen.de/desksharingtool-dev-frontend:latest
+#lcr.justiz.sachsen.de/lit-apps/sample-app:1.0.3
+#curl http://localhost:5000/v2/_catalog
 scp desksharingtool_dev-*.bin adm.rlehmann@jus-as-rz12:/home/adm.rlehmann/desksharing
